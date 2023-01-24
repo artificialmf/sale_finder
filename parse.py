adad61e07facfdb7b0f745637b1434b5caf1eb19
vkid <ayrat2005@gmail.com>
2023-01-24 10:27:41 +0300

    added nav, search bar, more parsing

--- parse.py
+++ parse.py
@@ -1,8 +1,9 @@
 # import requests
 # from bs4 import BeautifulSoup
+from time import sleep
 
 # urls = ['https://www.dns-shop.ru/search/?q=iphone']
-# headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"}
+headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"}
 
 # def get_info(searchreq):
 #     response = []
@@ -26,28 +27,74 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.options import Options
 from bs4 import BeautifulSoup
+import requests
 
-Options().add_argument('log-level=3')
+Options().add_argument('--disable-logging')
 
-def get_info(sr):                            #search request
+def get_info(sr): 
+    sr = sr.replace(' ', '+')                           #search request
     response_ = []
     driver = webdriver.Edge('driver.exe') 
-    driver.get('https://www.dns-shop.ru/search/?q=' + sr)
+    
+    driver.get('https://www.dns-shop.ru/search/?q=' + sr)                     #start dns
     products = driver.find_elements(By.CLASS_NAME, 'catalog-product')
     for product in products:
         title = product.find_element(By.CLASS_NAME,'catalog-product__name').text
         price = product.find_element(By.CLASS_NAME,'product-buy__price').text.split('\n')[0]
         rating = product.find_element(By.CLASS_NAME,'catalog-product__rating').get_attribute('data-rating')
-        img = product.find_element(By.CLASS_NAME, 'catalog-product__image-link').get_attribute('innerHTML')
         img = BeautifulSoup(product.find_element(By.CLASS_NAME, 'catalog-product__image-link').get_attribute('innerHTML')).find('img')['data-src']
 
-        response_.append({'title': title, 'price': price, 'rating': rating, 'img': img, 'store':'dns'})
+        response_.append({'title': title, 'price': price, 'rating': rating, 'img': img, 'store':'dns'})                        #end dns
     
     
     driver.get('https://www.citilink.ru/search/?text=' + sr)
-    
+    products = BeautifulSoup(driver.page_source, 'html.parser').find_all('div', class_='ProductCardVertical')    #print(BeautifulSoup(page.text, 'html.parser'))
+    for product in products:
+        title = product.find('a', class_='ProductCardVertical__name').text
+        price = product.find('span', class_='ProductCardVerticalPrice__price-current_current-price').text.replace(' ', '').replace('\n', '')
+        try:
+            rating = product.find('span', class_='ProductCardVerticalMeta__count ').text
+        except Exception:
+            rating = ''
+        img = product.find('div', class_='ProductCardVertical__picture-hover_part')['data-src']
+        response_.append({'title': title, 'price': price, 'rating': rating, 'img': img, 'store':'citilink'})
+
+    # page = requests.get('https://shop.mts.ru/search/?TYPE=products&q=' + sr, headers=headers)
+
+
+    # products = BeautifulSoup(page.text, 'html.parser').find_all('div', 'card-product-wrapper')
+    # print(BeautifulSoup(page.text, 'html.parser'))
+    # for product in products:
+    #     if product.find('button', class_='buy-button')['style'] == 'display:none;':
+    #         pass
+    #     else:
+    #         title = product.find('span', class_='shaved-text__original-text').text
+    #         price = product.find('span', class_='product-price__current').text
+    #         rating = product.find('span', class_='assessment-product__text').text
+    #         img = product.find('div', class_='gallery').find('img')['src']
+    #     response_.append({'title': title, 'price': price, 'rating': rating, 'img': img, 'store':'mts'})
+
+    driver.get('https://re-store.ru/search/?q='+sr)
+    producrs = []
+    while True:
+        try:
+            print(driver.find_element(By.CLASS_NAME, 'ddl_product').get_attribute('innerHTML'))
+            break
+
+        except Exception:
+            pass
+    products = driver.find_elements(By.CLASS_NAME, 'ddl_product')
+    print(products)
+    for product in products:
+        title = product.find_element(By.CLASS_NAME, 'catalog__item-title').text
+        price = product.find_element(By.CLASS_NAME, 'actual-price').text
+        rating = ''
+        img = product.find_element(By.CLASS_NAME, 'catalog__item-img-link').find_element(By.CLASS_NAME, 'lazyload').get_attribute('src')
+        response_.append({'title': title, 'price': price, 'rating': rating, 'img': img, 'store':'restore'})
+
     return response_
 
-print(get_info('iphone'))
+for i in get_info('iphone'):
+    print(i)
 #/div[1]/a/picture/img
 #/html/body/div[2]/div/div[3]/div[2]/div[2]/div/div[1]/div[1]/div[1]/a
